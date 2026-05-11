8882c0945093ec963360cc08913ed807706e430c
ezekiel <favourtobiloba200@gmail.com>
2026-05-11 14:23:52 +0100

    Context Api with state management

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import authContext from '../context/authContext';
 
 export const Home = () => {
     const [count, setCount] = useState(1);
     const [monitor, setMonitor] = useState(true);
+    const {name, email, setCart, cart}= useContext(authContext)
 
     useEffect(() => {
         const Timer = setInterval(() => {
@@ -20,7 +22,11 @@ export const Home = () => {
 
     return (
         <div>Home {count}
+        <p>name{name}</p>
+        <p>Email{email}</p>
             <button onClick={() => setMonitor(prev => !prev)}>Run</button>
+             <button onClick={()=> setCart(cart + 1)}>UpdateCart</button>
         </div>
+       
     )
 }
